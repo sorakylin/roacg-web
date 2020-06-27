841c9769c9d8eff7932a143d241ef652dd2cede4
skypyb <pyb068@gmail.com>
2020-06-27 14:44:32 +0800

    创建团队的弹框/表单完成

--- src/views/tc/TcMy.vue
+++ src/views/tc/TcMy.vue
@@ -13,7 +13,7 @@
         </span>
         <span>
           <a-icon type="project" />
-          参与项目数: {{this.myAllTeamTotal}}
+          参与项目数: {{this.myAllProjectTotal}}
         </span>
       </div>
     </a-col>
@@ -35,19 +35,48 @@
       :maskClosable="false"
       :keyboard="false"
       @cancel="handleCreateTeamCancel"
-      @create="handleCreateTeamSubmit"
+      @ok="handleCreateTeamSubmit"
     >
       <!-- 创建团队的表单 -->
-      <a-form-model ref="createTeamForm" :model="createTeamFormData" :rules="createTeamFormRules">
-        <a-form-model-item prop="teamName">
-          <a-input v-model="createTeamFormData.teamName" placeholder="请输入团队名" />
+      <a-form-model
+        ref="createTeamForm"
+        :model="createTeamFormData"
+        :rules="createTeamFormRules"
+        :label-col="createTeamLabelCol"
+        :wrapper-col="createTeamWrapperCol"
+      >
+        <a-form-model-item prop="teamName" label="团队名称">
+          <a-input v-model="createTeamFormData.teamName" placeholder="请输入团队名称" />
         </a-form-model-item>
-        <a-form-model-item prop="teamProfile">
+        <a-form-model-item label="团队描述" prop="teamProfile">
           <a-input-password
             type="textarea"
             v-model="createTeamFormData.teamProfile"
-            placeholder="团队描述"
+            placeholder="请输入团队描述"
           />
+        </a-form-model-item>
+        <a-form-model-item label="团队图像" prop="avatar">
+          <a-upload
+            name="avatar"
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            list-type="picture-card"
+            :show-upload-list="false"
+            :before-upload="beforeUpload"
+            @change="handleUploadChange"
+            style="width:100%"
+          >
+            <img
+              v-if="createTeamFormData.avatar != undefined && createTeamFormData.avatar!=''"
+              :src="createTeamFormData.avatar"
+              alt="avatar"
+              width="300px"
+              height="80px"
+            />
+            <div v-else>
+              <a-icon :type="createTeamUploadLoading ? 'loading' : 'plus'" />
+              <div class="ant-upload-text">Upload</div>
+            </div>
+          </a-upload>
         </a-form-model-item>
       </a-form-model>
     </a-modal>
@@ -60,62 +89,114 @@
 import TcTeamsApi from "@/api/tc/TcTeamsApi";
 import TcTempApi from "@/api/tc/TcTempApi";
 
+function getBase64(img, callback) {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result));
+  reader.readAsDataURL(img);
+}
+
 export default {
   name: "TcMy",
   data() {
     return {
-      createTeamVisible: false,
-      createTeamFormData: { teamName: "", avatar: "", teamProfile: "" },
+      createTeamUploadLoading: false, //创建团队的弹框里的上传图片是否加载
+      createTeamLabelCol: { span: 6 },
+      createTeamWrapperCol: { span: 16 },
+      createTeamVisible: false, //创建团队表单显隐
+      createTeamFormData: { teamName: "", avatar: "", teamProfile: "" }, //创建团队的表单内容
       createTeamFormRules: {
+        //创建团队表单效验规则
         teamName: [
-          { required: true, message: "团队名为必填项", trigger: "change" },
+          { required: true, message: "团队名为必填项", trigger: "blur" },
           { min: 3, message: "团队名长度最小为3", trigger: "change" },
           { max: 16, message: "团队名长度超过限制", trigger: "change" }
         ]
       },
-      myAllTeamTotal: 0,
-      myTeams: []
+      myAllTeamTotal: 0, //我加入的所有团队
+      myTeams: [],
+      myAllProjectTotal: 0 //我参与的所有项目
     };
   },
   created() {
-    TcTeamsApi.findMyTeams()
-      .then(res => {
-        if (!res.data.success) {
-          this.$message.error(res.message);
-          return;
-        }
-
-        let result = res.data;
-
-        //渲染我加入的项目组栏位
-        this.myAllTeamTotal = result.total ? result.total : 0;
-        this.myTeams = result.content;
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    this.pageInit();
   },
   methods: {
+    pageInit() {
+      TcTeamsApi.findMyTeams()
+        .then(res => {
+          if (!res.data.success) {
+            this.$message.error(res.message);
+            return;
+          }
+
+          let result = res.data.data;
+
+          //渲染我加入的项目组栏位
+          this.myAllTeamTotal = result.total ? result.total : 0;
+          this.myTeams = result.content;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
     showCreateTeamModal() {
       this.createTeamVisible = true;
     },
     handleCreateTeamCancel() {
+      this.$refs["createTeamForm"].resetFields();
       this.createTeamVisible = false;
     },
+    /**
+     * 提交表单 (创建团队)
+     */
     handleCreateTeamSubmit() {
       const form = this.$refs["createTeamForm"];
       form.validate(valid => {
         if (!valid) return;
 
+        //因为还没做图片上传, 这里先把base64地址去掉
+        this.createTeamFormData.avatar = undefined;
         TcTempApi.createTeam(this.createTeamFormData).then(res => {
-          if (!res.data.success) {//提交失败
-        
-            this.$message.error(res.data.message);
+          if (!res.data.success) {
+            //提交失败
+
+            this.$message.error(res.data.msg);
           }
           form.resetFields();
+          this.pageInit(); //重刷页面
           this.createTeamVisible = false;
         });
       });
+    },
+    /**
+     * 创建表单弹框里图片上传事件
+     */
+    handleUploadChange(info) {
+      console.log(info.file.status);
+      if (info.file.status === "uploading") {
+        this.createTeamUploadLoading = true;
+        return;
+      }
+      if (info.file.status === "done") {
+        getBase64(info.file.originFileObj, imageUrl => {
+          this.createTeamFormData.avatar = imageUrl;
+          this.createTeamUploadLoading = false;
+        });
+      }
+    },
+    //过滤创建表单弹框的图片上传
+    beforeUpload(file) {
+      const isJpgOrPng =
+        file.type === "image/jpeg" || file.type === "image/png";
+      if (!isJpgOrPng) {
+        this.$message.error("You can only upload JPG file!");
+      }
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isLt2M) {
+        this.$message.error("Image must smaller than 2MB!");
+      }
+
+      return isJpgOrPng && isLt2M;
     }
   }
 };
